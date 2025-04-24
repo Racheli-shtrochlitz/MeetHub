3d65a6d70db61ea8119c6eb5909293c20429a32e
ester-meyer <mindy05832@gmail.com>
2025-04-24 16:59:17 +0300

    unChange-role-field

--- server/Models/user.js
+++ server/Models/user.js
@@ -5,16 +5,12 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },  // הצפנת סיסמאות
-<<<<<<< HEAD
-  isTeacher: { type: Boolean, required: true },
-=======
   roles: {
     type: [String],
     enum: ['teacher', 'student'],
     default: [],
   },  
   activeRole:{type:String,enum: ['teacher', 'student']},
->>>>>>> b0ebece9bcc4beabcdb7c892bac528580b53ab10
   createdAt: { type: Date, default: Date.now },
   student: { type: mongoose.Schema.types.objectId, required: true},
 });
